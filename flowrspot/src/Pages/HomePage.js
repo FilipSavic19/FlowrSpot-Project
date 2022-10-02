8995534cc3741e05ae15fb86ba6758a1d3662c91
FilipSavic19 <filipsavic369@gmail.com>
2022-10-02 16:05:16 +0200

    init

--- flowrspot/src/Pages/HomePage.js
+++ flowrspot/src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import Hero from "../Images/pl-hero.png";
+import SearchIcon from "../Images/Vector 3.png";
 import "../Styles/HomePage.css";
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
           <h1>Discover flowers around you</h1>
           <p className="explore">Explore between more than 8.427 sightings</p>
           <input placeholder="Looking for something specific?" />
+          <img className="hero-image-search-image" src={SearchIcon} />
         </div>
       </div>
     </div>
