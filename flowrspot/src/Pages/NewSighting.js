ccf73f1f626663df0feb3d345e66a490ec82dbef
FilipSavic19 <filipsavic369@gmail.com>
2022-10-29 19:01:45 +0200

    new sighting

--- flowrspot/src/Pages/NewSighting.js
+++ flowrspot/src/Pages/NewSighting.js
@@ -2,6 +2,8 @@ import React from "react";
 import ViewOnGoogleMaps from "../components/Buttons/ViewOnGoogleMaps";
 import Map from "../components/Map";
 import "../Styles/NewSighting.css";
+import AddPhotoButton from "../components/Buttons/AddPhotoButton";
+import CreateNewSightingButton from "../components/Buttons/CreateNewSightingButton";
 
 const NewSighting = () => {
   return (
@@ -17,6 +19,23 @@ const NewSighting = () => {
           <div className="newSightingTitle">
             <h3>Add New Sighting</h3>
             <p>Explore between more than 8247 sightings</p>
+          </div>
+          <div className="inputs-and-button">
+            <div className="inputOne">
+              <input placeholder="Title of the sighting" />
+            </div>
+            <div className="inputTwo">
+              <input placeholder="Coordinates of the sighting" />
+            </div>
+            <div className="addPhotoBtn">
+              <AddPhotoButton />
+            </div>
+          </div>
+          <div className="inputThree">
+            <input placeholder="Write a description..." />
+          </div>
+          <div className="cnaButton">
+            <CreateNewSightingButton />
           </div>
         </div>
       </div>
