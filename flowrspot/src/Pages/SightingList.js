da7857f9de9df6ff48a485f3dc05f3c2686f732b
FilipSavic19 <filipsavic369@gmail.com>
2022-11-15 16:28:24 +0100

    sighting list

--- flowrspot/src/Pages/SightingList.js
+++ flowrspot/src/Pages/SightingList.js
@@ -1,21 +1,41 @@
 import React from "react";
+import AddNewSightingButton from "../components/Buttons/AddNewSightingButton";
 import CardSighting from "../components/CardSighting";
+import "../Styles/SightingList.css";
 
 const SightingList = () => {
   return (
-    <div className="sighting-list">
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
-      <CardSighting />
+    <div className="sightingList">
+      <div className="sightingListTopPart">
+        <div className="sightingTitleButton">
+          <div className="sightingTitle">
+            <p>Sighting List</p>
+          </div>
+          <div className="sightingButtonAdd">
+            <AddNewSightingButton />
+          </div>
+        </div>
+        <div className="sightingListDesc">
+          <p>Explore between more than 8427 sightings</p>
+        </div>
+      </div>
+      <div className="sighting-list">
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+        <CardSighting />
+      </div>
+      <div className="sightingBtnAdd">
+        <AddNewSightingButton />
+      </div>
     </div>
   );
 };
---
+++ flowrspot/src/components/Buttons/AddNewSightingButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Styles/AddNewSightingButton.css";
+
+const AddNewSightingButton = () => {
+  return (
+    <div className="addNewSightingButton">
+      <button>+ Add New Sighting </button>
+    </div>
+  );
+};
+
+export default AddNewSightingButton;
